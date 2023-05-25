631978a0529542727e0a5a30ea08941e2a833324
Viznem <thinhtruong3344@gmail.com>
2023-05-25 11:01:52 +0700

    Add products api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -11,12 +12,19 @@ export class AuthService {
         private usersRepository: Repository<User>,
      ){}
 
+     private async hashPassword(password: string, salt: string): Promise<string> {
+      return bcrypt.hash(password, salt);
+     }
+
      async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
         const user = new User();
         user.username = username;
-        user.password = password;
+        user.salt = await bcrypt.genSalt();
+        user.password = await this.hashPassword(password, user.salt);
+
+        console.log(user.password);
         
         try{
          await this.usersRepository.save(user);
